d7f2034286af059e60a2d53d843aa0aa538da933
acalinux86 <aacali6777@gmail.com>
2025-08-21 22:30:09 +0300

    Error Fixes
    Also added all standard opcode set but not yet implemented
    Cleaned up the opcode lookup table

--- src/6502emulator.h
+++ src/6502emulator.h
@@ -40,15 +40,89 @@ typedef enum {
 } Addressing_Modes;
 
 typedef enum {
+    // System Functions
     BRK,
+    NOP,
+    RTI,
+
+    // Jump and calls
     RTS,
+    JMP,
+    JSR,
 
+    // Arithmetic
     ADC,
+    SBC,
+    CMP,
+    CPX,
+    CPY,
 
+    // Load Operations
     LDA,
-    CLC,
+    LDY,
+    LDX,
 
+    // Store Operations
     STA,
+    STY,
+    STX,
+
+    // Clear Status Flag Changes
+    CLC,
+    CLV,
+    CLI,
+    CLD,
+
+    // Set Status Flag Changes
+    SEC,
+    SED,
+    SEI,
+
+    // Logical
+    ORA,
+    AND,
+    EOR,
+    BIT,
+
+    // Register Transfers
+    TAX,
+    TAY,
+    TXA,
+    TYA,
+
+    // Stack Operations
+    TSX,
+    TXS,
+    PHA,
+    PHP,
+    PLA,
+    PLP,
+
+    // Increments
+    INC,
+    INX,
+    INY,
+
+    // Decrements
+    DEX,
+    DEY,
+    DEC,
+
+    // Branches
+    BNE,
+    BCC,
+    BCS,
+    BEQ,
+    BMI,
+    BPL,
+    BVC,
+    BVS,
+
+    // Shifts
+    ASL,
+    LSR,
+    ROL,
+    ROR,
 
     // Error Values
     ERROR_FETCH_DATA,
@@ -98,24 +172,24 @@ typedef struct {
     Addressing_Modes mode;
 } Opcode_Info;
 
-Opcode_Info opcode_matrix[MAX_U8] = {
+Opcode_Info opcode_matrix[MAX_U8 + 1] = {
     //-0                  -1          -2            -3           -4         -5         -6           -7           -8          -9           -A              -B          -C          -D          -E          -F
-    {BRK, IMPL}, {ORA, INDX},        0X02,        0X03,        0X04, {ORA, ZP} , {ASL, ZP},         0X07, {PHP, IMPL}, {ORA, IMME}, {ASL, ACCU},        0X0B,        0X0C,  {ORA, ABS},  {ASL, ABS},   0X0F, // 0-
-    {BPL, REL} , {ORA, INDY},        0X12,        0X13,        0X14, {ORA, ZPX}, {ASL, ZPX},        0X17, {CLC, IMPL}, {ORA, ABSY},        0X1A,        0X1B,        0X1C, {ORA, ABSX}, {ASL, ABSX},   0X1F, // 1-
-    {JSR, ABS} , {AND, INDX},        0X22,        0X23, {BIT, ZP}  , {AND, ZP} , {ROL, ZP},         0X27, {PLP, IMPL}, {AND, IMME}, {ROL, ACCU},        0X2B,  {BIT, ABS},  {AND, ABS},  {ROL, ABS},   0X2F, // 2-
-    {BMI, REL} , {AND, INDY},        0X32,        0X33,        0X34, {AND, ZPX}, {ROL, ZPX},        0X37, {SEC, IMPL}, {AND, ABSY},        0X3A,        0X3B,        0X3C, {AND, ABSX}, {ROL, ABSX},   0X3F, // 3-
-    {RTI, IMPL}, {EOR, INDX},        0X42,        0X43,        0X44, {EOR, ZP} , {LSR, ZP},         0X47, {PHA, IMPL}, {EOR, IMME}, {LSR, ABS},         0X4B,  {JMP, ABS},  {EOR, ABS},  {LSR, ABS},   0X4F, // 4-
-    {BVC, REL} , {EOR, INDY},        0X52,        0X53,        0X54, {EOR, ZPX}, {LSR, ZPX},        0X57, {CLI, IMPL}, {EOR, ABSY},        0X5A,        0X5B,        0X5C, {EOR, ABSX}, {LSR, ABSX},   0X5F, // 5-
-    {RTS, IMPL}, {ADC, INDX},        0X62,        0X63,        0X64, {ADC, ZP} , {ROR, ZP},         0X67, {PLA, IMPL}, {ADC, IMME}, {ROR, ACCU},        0X6B,  {JMP, IND},  {ADC, ABS},  {ROR, ABS},   0X6F, // 6-
-    {BVS, REL} , {ADC, INDY},        0X72,        0X73,        0X74, {ADC, ZPX}, {ROR, ZPX},        0X77, {SEI, IMPL}, {ADC, ABSY},        0X7A,        0X7B,        0X7C, {ADC, ABSX}, {ROR, ABSX},   0X7F, // 7-
-           0X80, {STA, INDX},        0X82,        0X83, {STY, ZP}  , {STA, ZP} , {STX, ZP},         0X87, {DEY, IMPL},        0X89, {TXA, IMPL},        0X8B,  {STY, ABS},  {STA, ABS},  {STX, ABS},   0X8F, // 8-
-    {BCC, REL} , {STA, INDY},        0X92,        0X93, {STY, ZPX} , {STA, ZPX}, {STX, ZPY},        0X97, {TYA, IMPL}, {STA, ABSY}, {TXS, IMPL},        0X9B,        0X9C, {STA, ABSX},        0X9E,   0X9F, // 9-
-    {LDY, IMME}, {LDA, INDX}, {LDX, IMME},        0XA3, {LDY, ZP}  , {LDA, ZP} , {LDX, ZP},         0XA7, {TAY, IMPL}, {LDA, IMME}, {TAX, IMPL},        0XAB,  {LDY, ABS},  {LDA, ABS},  {LDX, ABS},   0XAF, // A-
-    {BCS, REL} , {LDA, INDY},        0XB2,        0XB3, {LDY, ZPX} , {LDA, ZPX}, {LDX, ZPY},        0XB7, {CLV, IMPL}, {LDA, ABSY}, {TSX, IMPL},        0XBB, {LDY, ABSX}, {LDA, ABSX}, {LDX, ABSY},   0XBF, // B-
-    {CPY, IMME}, {CMP, INDX},        0XC2,        0XC3, {CPY, ZP}  , {CMP, ZP} , {DEC, ZP},         0XC7, {INY, IMPL}, {CMP, IMME}, {DEX, IMPL},        0XCB,  {CPY, ABS},  {CMP, ABS},  {DEC, ABS},   0XCF, // C-
-    {BNE, REL} , {CMP, INDY},        0XD2,        0XD3,        0XD4, {CMP, ZPX}, {DEC, ZPX},        0XD7, {CLD, IMPL}, {CMP, ABSY},        0XDA,        0XDB,        0XDC, {CMP, ABSX}, {DEC, ABSX},   0XDF, // D-
-    {CPX, IMME}, {SBC, INDX},        0XE2,        0XE3, {CPX, ZP}  , {SBC, ZP} , {INC, ZP},         0XE7, {INX, IMPL}, {SBC, IMME}, {NOP, IMPL},        0XEB,  {CPX, ABS},  {SBC, ABS},  {INC, ABS},   0XEF, // E-
-    {BEQ, REL} , {SBC, INDY},        0XF2,        0XF3,        0XF4, {SBC, ZPX}, {INC, ZPX},        0XF7, {SED, IMPL}, {SBC, ABSY},        0XFA,        0XFB,        0XFC, {SBC, ABSX}, {INC, ABSX},   0XFF, // F-
+    {BRK, IMPL}, {ORA, INDX},        {0x00},        {0x00},        {0x00}, {ORA, ZP} ,  {ASL, ZP},         {0x00}, {PHP, IMPL}, {ORA, IMME},   {ASL, ACCU},        {0x00},      {0x00},  {ORA, ABS},  {ASL, ABS},   {0x00}, // 0-
+    {BPL, REL} , {ORA, INDY},        {0x00},        {0x00},        {0x00}, {ORA, ZPX}, {ASL, ZPX},        {0x00}, {CLC, IMPL}, {ORA, ABSY},        {0x00},        {0x00},      {0x00}, {ORA, ABSX},  {ASL, ABSX},   {0x00}, // 1-
+    {JSR, ABS} , {AND, INDX},        {0x00},        {0x00},     {BIT, ZP}, {AND, ZP} ,  {ROL, ZP},         {0x00}, {PLP, IMPL}, {AND, IMME},    {ROL, ACCU},       {0x00},  {BIT, ABS},  {AND, ABS},  {ROL, ABS},   {0x00}, // 2-
+    {BMI, REL} , {AND, INDY},        {0x00},        {0x00},        {0x00}, {AND, ZPX}, {ROL, ZPX},        {0x00}, {SEC, IMPL}, {AND, ABSY},        {0x00},        {0x00},      {0x00}, {AND, ABSX},  {ROL, ABSX},   {0x00}, // 3-
+    {RTI, IMPL}, {EOR, INDX},        {0x00},        {0x00},        {0x00}, {EOR, ZP} ,  {LSR, ZP},         {0x00}, {PHA, IMPL}, {EOR, IMME},    {LSR, ABS},        {0x00},  {JMP, ABS},  {EOR, ABS},  {LSR, ABS},   {0x00}, // 4-
+    {BVC, REL} , {EOR, INDY},        {0x00},        {0x00},        {0x00}, {EOR, ZPX}, {LSR, ZPX},        {0x00}, {CLI, IMPL}, {EOR, ABSY},        {0x00},        {0x00},      {0x00}, {EOR, ABSX},  {LSR, ABSX},   {0x00}, // 5-
+    {RTS, IMPL}, {ADC, INDX},        {0x00},        {0x00},        {0x00}, {ADC, ZP} ,  {ROR, ZP},         {0x00}, {PLA, IMPL}, {ADC, IMME},   {ROR, ACCU},        {0x00},  {JMP, IND},  {ADC, ABS},  {ROR, ABS},  {0x00}, // 6-
+    {BVS, REL} , {ADC, INDY},        {0x00},        {0x00},        {0x00}, {ADC, ZPX}, {ROR, ZPX},        {0x00}, {SEI, IMPL}, {ADC, ABSY},        {0x00},        {0x00},      {0x00}, {ADC, ABSX},  {ROR, ABSX},   {0x00}, // 7-
+         {0x00}, {STA, INDX},        {0x00},        {0x00},     {STY, ZP}, {STA, ZP} ,  {STX, ZP},         {0x00}, {DEY, IMPL},        {0x00}, {TXA, IMPL},        {0x00},  {STY, ABS},  {STA, ABS},  {STX, ABS},  {0x00}, // 8-
+    {BCC, REL} , {STA, INDY},        {0x00},        {0x00},    {STY, ZPX}, {STA, ZPX}, {STX, ZPY},        {0x00}, {TYA, IMPL}, {STA, ABSY},   {TXS, IMPL},        {0x00},      {0x00}, {STA, ABSX},       {0x00},   {0x00}, // 9-
+    {LDY, IMME}, {LDA, INDX},   {LDX, IMME},        {0x00},     {LDY, ZP}, {LDA, ZP} ,  {LDX, ZP},         {0x00}, {TAY, IMPL}, {LDA, IMME},   {TAX, IMPL},        {0x00},  {LDY, ABS},  {LDA, ABS},  {LDX, ABS},   {0x00}, // A-
+    {BCS, REL} , {LDA, INDY},        {0x00},        {0x00},    {LDY, ZPX}, {LDA, ZPX}, {LDX, ZPY},        {0x00}, {CLV, IMPL}, {LDA, ABSY},   {TSX, IMPL},        {0x00}, {LDY, ABSX}, {LDA, ABSX},  {LDX, ABSY},    {0x00}, // B-
+    {CPY, IMME}, {CMP, INDX},        {0x00},        {0x00},     {CPY, ZP}, {CMP, ZP} ,  {DEC, ZP},         {0x00}, {INY, IMPL}, {CMP, IMME},   {DEX, IMPL},        {0x00},  {CPY, ABS},  {CMP, ABS},  {DEC, ABS},   {0x00}, // C-
+    {BNE, REL} , {CMP, INDY},        {0x00},        {0x00},        {0x00}, {CMP, ZPX}, {DEC, ZPX},        {0x00}, {CLD, IMPL}, {CMP, ABSY},        {0x00},        {0x00},      {0x00}, {CMP, ABSX},  {DEC, ABSX},    {0x00}, // D-
+    {CPX, IMME}, {SBC, INDX},        {0x00},        {0x00},     {CPX, ZP}, {SBC, ZP} ,  {INC, ZP},         {0x00}, {INX, IMPL}, {SBC, IMME},   {NOP, IMPL},        {0x00},  {CPX, ABS},  {SBC, ABS},  {INC, ABS},   {0x00}, // E-
+    {BEQ, REL} , {SBC, INDY},        {0x00},        {0x00},        {0x00}, {SBC, ZPX}, {INC, ZPX},        {0x00}, {SED, IMPL}, {SBC, ABSY},        {0x00},        {0x00},      {0x00}, {SBC, ABSX},  {INC, ABSX},    {0x00}, // F-
 };
 
 #endif // EMULATOR_6502_H_
