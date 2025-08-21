ae54a56b7bb94d6d434d37015586eb43b313a3a1
acalinux86 <aacali6777@gmail.com>
2025-08-21 13:41:30 +0300

    Implement LDA

--- src/6502emulator.c
+++ src/6502emulator.c
@@ -2,12 +2,15 @@
 #include <stdlib.h>
 #include <assert.h>
 #include <stdint.h>
+#include <stdbool.h>
 
 #include "./6502emulator.h"
 
+#define MAX_CAPACITY_OF_EACH_PAGE 0x100 // 256
+#define NUMBER_OF_PAGES 0x100 // 256
+
 // Global Variables
 Memory memory[NUMBER_OF_PAGES][MAX_CAPACITY_OF_EACH_PAGE] = {0X00}; // Memory Layout
-Zero_Page *zero_page = memory[0x00]; // Page 0 (Zero Page)
 u8 stack_size = 0x00; // We wanna Track the stack Size
 Stack *stack = memory[0x01]; // Page 1
 
@@ -18,18 +21,29 @@ Register_Y Y = 0x00;
 PC program_counter = 0x00;
 PSR processor_status_register = 0x00;
 
+const char *s502_addr_mode_as_cstr(Addressing_Modes mode);
+const char *s502_opcode_as_cstr(Opcode opcode);
+const char *s502_operand_type_as_cstr(Operand_Type type);
+
 #define UNIMPLEMENTED(message)                                         \
     do {                                                               \
         fprintf(stderr, "ERROR: %s not implement yet!!!\n", message) ; \
         exit(1);                                                       \
     } while (0)
 
-const char *s502_opcode_as_cstr(Opcode opcode);
-#define ILLEGAL_ADDRESSING(mode, opcode)                            \
-    do {                                                            \
-        fprintf(stderr, "ERROR: Invalid `mode` mode on %s\n",       \
-                s502_opcode_as_cstr(opcode));                       \
-        exit(1);                                                    \
+#define ILLEGAL_ADDRESSING(mode, opcode)                    \
+    do {                                                    \
+        fprintf(stderr, "ERROR: Invalid `%s` mode on %s\n", \
+                s502_addr_mode_as_cstr(mode),               \
+                s502_opcode_as_cstr(opcode));               \
+        exit(1);                                            \
+    } while (0)
+
+#define ILLEGAL_ACCESS(mode, operand)                           \
+    do {                                                        \
+        fprintf(stderr, "ERROR: Invalid `%s` type on `%s` mode. \n",\
+                s502_operand_type_as_cstr(operand),             \
+                s502_addr_mode_as_cstr(mode));                  \
     } while (0)
 
 void s502_dump_page(u8 *page)
@@ -88,7 +102,7 @@ u8 s502_read_memory(Location location)
     return memory[location.page][location.offset];
 }
 
-void s502_write_memory(Location location, Data data)
+void s502_write_memory(Location location, u8 data)
 {
     memory[location.page][location.offset] = data;
 }
@@ -109,8 +123,7 @@ const char *s502_addr_mode_as_cstr(Addressing_Modes mode)
     case INDIRECT:         return "INDIRECT";
     case INDEXED_INDIRECT: return "INDEXED_INDIRECT";
     case INDIRECT_INDEXED: return "INDIRECT_INDEXED";
-    default:
-        return NULL;
+    default:               return NULL;
     }
 }
 
@@ -123,8 +136,16 @@ const char *s502_opcode_as_cstr(Opcode opcode)
     case LDA: return "LDA";
     case STA: return "STA";
     case CLC: return "CLC";
-    default:
-        return NULL;
+    default:  return NULL;
+    }
+}
+const char *s502_operand_type_as_cstr(Operand_Type type)
+{
+    switch (type) {
+    case OPERAND_ABSOLUTE: return "OPERAND_ABSOLUTE";
+    case OPERAND_LOCATION: return "OPERAND_LOCATION";
+    case OPERAND_DATA:     return "OPERAND_DATA";
+    default:               return NULL;
     }
 }
 
@@ -133,18 +154,18 @@ void s502_clear_carry_bit()
     s502_set_psr_flags(C_BIT_FLAG & 0);
 }
 
-void s502_store_accumulator(Operand operand)
-{
-    s502_write_memory(operand.location, accumulator);
-    accumulator = 0x00;// clear the accumulator
-}
+// void s502_store_accumulator(Operand operand)
+// {
+//     s502_write_memory(operand.location, accumulator);
+//     accumulator = 0x00;// clear the accumulator
+// }
 
-void s502_add_with_carry(Operand operand)
-{
-    u8 value = s502_read_memory(operand);
-    accumulator += value;
-    if (value >= 255) s502_set_psr_flags(C_BIT_FLAG);
-}
+// void s502_add_with_carry(Operand operand)
+// {
+//     u8 value = s502_read_memory(operand.location);
+//     accumulator += value;
+//     if (value >= 255) s502_set_psr_flags(C_BIT_FLAG);
+// }
 
 void s502_set_accumulator(u8 data)
 {
@@ -152,66 +173,107 @@ void s502_set_accumulator(u8 data)
     if (accumulator == 0)        s502_set_psr_flags(Z_BIT_FLAG);
     if ((accumulator << 7) != 0) s502_set_psr_flags(N_BIT_FLAG);
     program_counter++;
-    return accumulator;
 }
 
+Location u16_to_loc(u16 sixteen_bit)
+{
+    // -- little-endian
+    return (Location) {
+        .page = sixteen_bit >> 8, // higher byte
+        .offset = sixteen_bit & 0xFF, // low-byte
+    };
+}
 
-// TODO: Find a Way to split a 16-bit data into page and offset
 // TODO: Refactor LDA to switch-case on operand types
 void s502_load_accumulator(Instruction instruction)
 {
-    UNIMPLEMENTED("s502_load_accumulator");
     switch (instruction.mode) {
     case IMMEDIATE: {
         // Immediate mode just loads a value into the accumulator
-        s502_set_accumulator(instruction.operand.data);
+        switch (instruction.operand.type) {
+        case OPERAND_DATA: {
+            u8 data = instruction.operand.data.data;
+            s502_set_accumulator(data);
+        } break;
+        default: ILLEGAL_ACCESS(instruction.mode, instruction.operand.type);
+        }
     } break;
 
     case ZERO_PAGE: {
-        // if the operand doesn't contain any page, assumed that it is page zero
-        assert(instruction.operand.location.page == 0x00 && "Invalid Page Zero Address");
-        u8 data = s502_read_memory(instruction.operand.location);
-        s502_set_accumulator(data);
+        switch (instruction.operand.type) {
+        case OPERAND_LOCATION: {
+            // if the operand doesn't contain any page, assumed that it is page zero
+            Location location = instruction.operand.data.address.loc;
+            assert(location.page == 0x00 && "Invalid Page Zero Address");
+            u8 data = s502_read_memory(location);
+            s502_set_accumulator(data);
+        } break;
+        default: ILLEGAL_ACCESS(instruction.mode, instruction.operand.type);
+        }
     } break;
 
     case ZERO_PAGE_X: {
         // zero page X modes sums x register to the zero page operand and uses it as index
         // It wraps around incase it exceeds page MAX_CAPACITY_OF_EACH_PAGE
-        assert(instruction.operand.location.page == 0x00 && "Invalid Page Zero Address");
-        u8 index = instruction.operand.location.offset + X;
-        u8 page = instruction.operand.location.page;
-        u8 data = s502_read_memory((Location) {.page = page, .offset = index});
-        s502_set_accumulator(data);
+        switch (instruction.operand.type) {
+        case OPERAND_LOCATION: {
+            Location location = instruction.operand.data.address.loc;
+            assert(location.page == 0x00 && "Invalid Page Zero Address");
+            u8 index = location.offset + X;
+            u8 page = location.page;
+            u8 data = s502_read_memory((Location) {.page = page, .offset = index});
+            s502_set_accumulator(data);
+        } break;
+        default: ILLEGAL_ACCESS(instruction.mode, instruction.operand.type);
+        }
     } break;
 
     case ABSOLUTE: {
         // uses the absolute location in operand to load access memory into accumulator
-        u8 data = s502_read_memory(instruction.operand.location);
-        s502_set_accumulator(data);
+        switch (instruction.operand.type) {
+        case OPERAND_ABSOLUTE: {
+            Absolute absolute = instruction.operand.data.address.absolute;
+            Location location = u16_to_loc(absolute);
+            u8 data = s502_read_memory(location);
+            s502_set_accumulator(data);
+        } break;
+        default: ILLEGAL_ACCESS(instruction.mode, instruction.operand.type);
+        }
     } break;
 
     case ABSOLUTE_X: {
-        // zero page x but absolute
-        u8 index = instruction.operand.location.offset + X;
-        if (index > 0xFF) instruction.operand.location.page++;
-        u8 page = instruction.operand.location.page;
-        u8 data = s502_read_memory((Location) {.page = page, .offset = index});
-        s502_set_accumulator(data);
+        // absolute but += X register
+        switch (instruction.operand.type) {
+        case OPERAND_ABSOLUTE: {
+            Absolute absolute = instruction.operand.data.address.absolute;
+            Absolute index = absolute + X;
+            Location location = u16_to_loc(index);
+            u8 data = s502_read_memory(location);
+            s502_set_accumulator(data);
+        } break;
+        default: ILLEGAL_ACCESS(instruction.mode, instruction.operand.type);
+        }
     } break;
 
     case ABSOLUTE_Y: {
-        // absolute x but y
-        u8 index = instruction.operand.location.offset + Y;
-        if (index > 0xFF) instruction.operand.location.page++;
-        u8 page = instruction.operand.location.page;
-        u8 data = s502_read_memory((Location) {.page = page, .offset = index});
-        s502_set_accumulator(data);
+        // absolute but += Y register
+        switch (instruction.operand.type) {
+        case OPERAND_ABSOLUTE: {
+            Absolute absolute = instruction.operand.data.address.absolute;
+            Absolute index = absolute + Y;
+            Location location = u16_to_loc(index);
+            u8 data = s502_read_memory(location);
+            s502_set_accumulator(data);
+        } break;
+        default: ILLEGAL_ACCESS(instruction.mode, instruction.operand.type);
+        }
     } break;
 
-        case
-    default: {
-        ILLEGAL_ADDRESSING(LDA);
-    }
+    case INDEXED_INDIRECT: {
+        // TODO:
+    } break;
+
+    default: ILLEGAL_ADDRESSING(instruction.mode, instruction.opcode);
     }
 }
 
@@ -226,22 +288,35 @@ bool s502_decode(Instruction instruction)
 {
     switch (instruction.opcode) {
     case LDA:
-        s502_load_accumulator(instruction); break;
+        s502_load_accumulator(instruction); return true;
     default:
-        ILLEGAL_ADDRESSING(instruction.mode, instruction.opcode);
+        return false;
     }
 }
 
 int main(void)
 {
-    // populate Memory
-    s502_write_memory((Operand){.page = 0x00, .offset = 0x65}, 0x0A);
-    s502_write_memory((Operand){.page = 0x00, .offset = 0x66}, 0x0A);
-    s502_clear_carry_bit();
-    s502_load_accumulator((Operand){.page = 0x00, .offset = 0x00});
-    s502_add_with_carry((Operand){.page = 0x00, .offset = 0x65});
-    s502_add_with_carry((Operand){.page = 0x00, .offset = 0x66});
-    s502_store_accumulator((Operand){.page = 0x00, .offset = 0x67});
-    s502_dump_page(zero_page);
+    u16 data = 0x1234;
+    Location location = u16_to_loc(data);
+
+    s502_write_memory(location, 0x0A);
+    Instruction instruction = {
+        .mode = ABSOLUTE,
+        .opcode = LDA,
+        .operand = {
+            .type = OPERAND_ABSOLUTE,
+            .data = {
+                .address = {
+                    .absolute = 0x1234
+                },
+            }
+        }
+    };
+
+    if (!s502_decode(instruction)) {
+        fprintf(stderr, "Failed to decode\n");
+        return 1;
+    }
+    s502_print_stats();
     return 0;
 }
