a4b4e055831987d4d0c7895ca609b3b3dec429e7
acalinx86 <aacali6777@gmail.com>
2025-09-15 21:06:52 +0300

    stage assembler and disassembler files

---
+++ src/mosasm.c
@@ -0,0 +1,9 @@
+// 6502 Assembler
+
+#include "./mos.h"
+
+// Provide details about the file in Usage
+int main(void)
+{
+    return 0;
+}
---
+++ src/mosdisasm.c
@@ -0,0 +1,10 @@
+// 6502 Disassembler
+
+#include "./mos.h"
+
+
+// Provide details about the file in Usage
+int main(void)
+{
+    return 0;
+}
