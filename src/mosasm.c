65932ce2a7d902bac35ec3bcba3ba6cf4fbe1f53
acalinx86 <aacali6777@gmail.com>
2025-09-23 11:04:56 +0300

    [mosasm] Assemler successfully lexes the test file

--- src/mosasm.c
+++ src/mosasm.c
@@ -9,7 +9,7 @@ char *mos_read_file(const char *file_path, uint64_t *size)
     FILE *fp = fopen(file_path, "rb");
     if (fp == NULL) {
         fprintf(stderr, "ERROR: file `%s` could not be opened because of : %s\n",
-                file_path, strerror(errno));
+        file_path, strerror(errno));
         return NULL;
     }
 
@@ -71,7 +71,7 @@ typedef struct {
     uint64_t lines;
     uint64_t cursor;
     uint64_t label_end; // store end of label
-    MOS_Tokens tokens;
+    MOS_Tokens tokens; // store tokens of parsed file
 } MOS_Lexer;
 
 const char *mos_token_type_as_cstr(MOS_TokenType type)
@@ -85,7 +85,7 @@ const char *mos_token_type_as_cstr(MOS_TokenType type)
     case MOS_TOKEN_OPCODE:     return "MOS_TOKEN_OPCODE";
     case MOS_TOKEN_DIRECTIVE:  return "MOS_TOKEN_DIRECTIVE";
     default:                   return NULL;
-    }
+}
 }
 
 bool mos_lexer_init(MOS_Lexer *lexer, const char *file_path)
@@ -110,28 +110,28 @@ bool mos_lexer_init(MOS_Lexer *lexer, const char *file_path)
     return true;
 }
 
-bool mos_lexer_is_whitespace(MOS_Lexer *lexer)
+bool mos_lexer_is_whitespace(const MOS_Lexer *lexer)
 {
     return lexer->content[lexer->cursor] == ' ' ||
-           lexer->content[lexer->cursor] == '\t';
+    lexer->content[lexer->cursor] == '\t';
 }
 
-bool mos_lexer_is_comment(MOS_Lexer *lexer)
+bool mos_lexer_is_comment(const MOS_Lexer *lexer)
 {
     return lexer->content[lexer->cursor] == ';';
 }
 
-bool mos_lexer_is_newline(MOS_Lexer *lexer)
+bool mos_lexer_is_newline(const MOS_Lexer *lexer)
 {
     return lexer->content[lexer->cursor] == '\n';
 }
 
-bool mos_lexer_is_eof(MOS_Lexer *lexer)
+bool mos_lexer_is_eof(const MOS_Lexer *lexer)
 {
     return lexer->content[lexer->cursor] == '\0';
 }
 
-char mos_lexer_peek(MOS_Lexer *lexer)
+char mos_lexer_peek(const MOS_Lexer *lexer)
 {
     // peek doesnt advance the cursor
     // it saves the cursor point and looks ahead
@@ -139,28 +139,28 @@ char mos_lexer_peek(MOS_Lexer *lexer)
     return lexer->content[next++];
 }
 
-bool mos_lexer_is_directive(MOS_Lexer *lexer)
+bool mos_lexer_is_directive(const MOS_Lexer *lexer)
 {
     return lexer->content[lexer->cursor] == '.';
 }
 
-bool mos_lexer_is_operand(MOS_Lexer *lexer)
+bool mos_lexer_is_operand(const MOS_Lexer *lexer)
 {
     return lexer->content[lexer->cursor] == '$';
 }
 
-bool mos_lexer_is_immediate(MOS_Lexer *lexer)
+bool mos_lexer_is_immediate(const MOS_Lexer *lexer)
 {
     return lexer->content[lexer->cursor] == '#';
 }
 
 MOS_Token mos_create_token(char *token, uint64_t len, MOS_TokenType type)
 {
-    return (MOS_Token) {
-        .token = mos_strdup(token, len),
-        .type = type,
-        .token_len = len,
-    };
+    MOS_Token _token = {0}; // zero initialize
+    _token.token = mos_strdup(token, len);
+    _token.type = type;
+    _token.token_len = len;
+    return _token;
 }
 
 void mos_lexer_append_token(MOS_Lexer *lexer, MOS_Token token)
@@ -177,59 +177,160 @@ char mos_lexer_get_char(MOS_Lexer *lexer)
 
 #define char_to_cstr(x) ((char[2]){x, '\0'})
 
-void mos_lexer_dump_tokens(MOS_Lexer *lexer)
+void mos_lexer_dump_tokens(const MOS_Lexer *lexer)
 {
     for (uint32_t i = 0; i < lexer->tokens.count; ++i) {
         fprintf(stdout, "%s(%s)\n", lexer->tokens.items[i].token, mos_token_type_as_cstr(lexer->tokens.items[i].type));
     }
 }
 
-// TODO: Provide details about the file in Usage
-int main(void)
+char *mos_to_upper(char *buffer)
 {
-    const char *file_path = "Test/s502_add.asm";
-    MOS_Lexer lexer;
-    if (!mos_lexer_init(&lexer, file_path)) return 1;
+    char buf[256] = {0};
+    uint64_t n = strlen(buffer);
+    for (uint64_t i = 0; i < n; ++i) {
+        // assert the char we are receiving is lower case
+        // maybe the assert is too strict
+        // TODO: resolve the assert
+        assert(buffer[i] >= 'a' && buffer[i] <= 'z');
+        buf[i] = buffer[i] - (97 - 65);
+    }
+    return mos_strdup(buf, n);
+}
+
+bool mos_is_alpha(char x)
+{
+    return (x >= 'A' && x <= 'Z') || (x >= 'a' && x <= 'z') || (x == '_');
+}
+
+bool mos_is_opcode(char *buf)
+{
+    const char *buffer = (const char*)mos_to_upper(buf);
+    const char *op_cstr[] = {
+        "BRK","NOP","RTI","RTS","JMP","JSR","ADC","SBC","CMP","CPX",
+        "CPY","LDA","LDY","LDX","STA","STY","STX","CLC","CLV","CLI",
+        "CLD","SEC","SED","SEI","ORA","AND","EOR","IT","TAX","TAY",
+        "TXA","TYA","TSX","TXS","PHA","PHP","PLA","PLP","INC","INX",
+        "INY","DEX","DEY","DEC","BNE","BCC","BCS","BEQ","BMI","BPL",
+        "BVC","BVS","ASL","LSR","ROL","ROR"
+    };
+
+    uint64_t count = MOS_ARRAY_LEN(op_cstr);
+    for (uint64_t i = 0; i < count; ++i) {
+        if (strcmp(op_cstr[i], buffer) == 0) {
+            return true;
+        }
+    }
+    return false;
+}
+
+bool mos_lexer_process_token_after_whitespace(MOS_Lexer **lexer, uint64_t *cursor, char *buf)
+{
+    if (lexer == NULL) return false;
+    MOS_Token token = {0};
+    buf[*cursor] = '\0';
+    if (*cursor > 0) {
+        //fprintf(stderr, "TODO: len: %ld, %s not catogorised yet\n",*cursor, buf);
+        if (mos_is_alpha(buf[0])) {
+            if (mos_is_opcode(buf)) {
+                token = mos_create_token(buf, *cursor, MOS_TOKEN_OPCODE);
+                mos_lexer_append_token(*lexer, token);
+            } else {
+                // TODO: if last token processed was a dot then directive
+                // else label
+                if ((*lexer)->tokens.items[(*lexer)->tokens.count - 1].type == MOS_TOKEN_DIRECTIVE) {
+                // printf("Last token processed type: %s\n", mos_token_type_as_cstr((*lexer)->tokens.items[(*lexer)->tokens.count - 1].type));
+                    token = mos_create_token(buf, *cursor, MOS_TOKEN_DIRECTIVE);
+                    mos_lexer_append_token(*lexer, token);
+                } else {
+                    token = mos_create_token(buf, *cursor, MOS_TOKEN_LABEL);
+                    mos_lexer_append_token(*lexer, token);
+                }
+            }
+        } else {
+            if ((*lexer)->tokens.items[(*lexer)->tokens.count - 2].type == MOS_TOKEN_IMMEDIATE) {
+                token = mos_create_token(buf, *cursor, MOS_TOKEN_IMMEDIATE);
+                mos_lexer_append_token(*lexer, token);
+            } else {
+                token = mos_create_token(buf, *cursor, MOS_TOKEN_OPERAND);
+                mos_lexer_append_token(*lexer, token);
+            }
+        }
+    }
+    *cursor = 0;
+    return true;
+}
+
+const char *mos_shift(int *argc, char ***argv)
+{
+    assert(*argc > 0);
+    const char *result = **argv;
+    (*argv)++;
+    (*argc)--;
+    return result;
+}
+
+void mos_usage(const char *program)
+{
+    fprintf(stderr, "MOS 6502 Assembler\n");
+    fprintf(stderr, "USAGE: %s <file_path>\n", program);
+}
+
+// TODO: Provide details about the file in Usage
+int main(int argc, char **argv)
+{
+    const char *program = mos_shift(&argc, &argv);
+    if (argc == 0) {
+        mos_usage(program);
+        return 1;
+    }
+
+    MOS_Lexer *lexer = malloc(sizeof(MOS_Lexer));
+    assert(lexer != NULL);
+    const char *file_path = mos_shift(&argc, &argv);
+    if (!mos_lexer_init(lexer, file_path)) return 1;
 
     char buf[256];
     uint64_t cursor = 0;
-    while (!mos_lexer_is_eof(&lexer)) {
-        if (mos_lexer_is_newline(&lexer)) {
-            lexer.cursor++; lexer.lines++;
+
+    while (!mos_lexer_is_eof(lexer)) {
+        MOS_Token token = {0};
+        if (mos_lexer_is_newline(lexer)) {
+            lexer->cursor++; lexer->lines++;
+            if (!mos_lexer_process_token_after_whitespace(&lexer, &cursor, buf)) return 1;
+        } else if (mos_lexer_is_whitespace(lexer)) {
+            lexer->cursor++;
+            if (!mos_lexer_process_token_after_whitespace(&lexer, &cursor, buf)) return 1;
+        } else if (mos_lexer_is_directive(lexer)) {
+            char *directive = char_to_cstr(mos_lexer_get_char(lexer));
+            token = mos_create_token(directive, 1, MOS_TOKEN_DIRECTIVE);
+            mos_lexer_append_token(lexer, token);
+        } else if (mos_lexer_is_immediate(lexer)) {
+            char *imme = char_to_cstr(mos_lexer_get_char(lexer));
+            token = mos_create_token(imme, 1, MOS_TOKEN_IMMEDIATE);
+            mos_lexer_append_token(lexer, token);
+        } else if (mos_lexer_is_operand(lexer)) {
+            char *operand = char_to_cstr(mos_lexer_get_char(lexer));
+            token = mos_create_token(operand, 1, MOS_TOKEN_OPERAND);
+            mos_lexer_append_token(lexer, token);
+        } else if (mos_lexer_is_comment(lexer)) {
+            while (mos_lexer_peek(lexer) != '\n') {
+                // for comment
+                buf[cursor++] = mos_lexer_get_char(lexer);
+            }
             buf[cursor] = '\0';
-            if (cursor > 0)
-                fprintf(stderr, "TODO: len: %ld, %s not catogorised yet\n",cursor, buf);
+            token = mos_create_token(buf, cursor, MOS_TOKEN_COMMENT);
+            mos_lexer_append_token(lexer, token);
             cursor = 0;
-        } else if (mos_lexer_is_whitespace(&lexer)) {
-            lexer.cursor++;
-            buf[cursor] = '\0';
-            if (cursor > 0)
-                fprintf(stderr, "TODO: len: %ld, %s not catogorised yet\n",cursor, buf);
-            cursor = 0;
-        } else if (mos_lexer_is_directive(&lexer)) {
-            char *directive = char_to_cstr(mos_lexer_get_char(&lexer));
-            MOS_Token token = mos_create_token(directive, 1, MOS_TOKEN_DIRECTIVE);
-            mos_lexer_append_token(&lexer, token);
-        } else if (mos_lexer_is_immediate(&lexer)) {
-            char *imme = char_to_cstr(mos_lexer_get_char(&lexer));
-            MOS_Token token = mos_create_token(imme, 1, MOS_TOKEN_IMMEDIATE);
-            mos_lexer_append_token(&lexer, token);
-        } else if (mos_lexer_is_operand(&lexer)) {
-            char *operand = char_to_cstr(mos_lexer_get_char(&lexer));
-            MOS_Token token = mos_create_token(operand, 1, MOS_TOKEN_OPERAND);
-            mos_lexer_append_token(&lexer, token);
-        } else if (mos_lexer_is_comment(&lexer)) {
-            while (mos_lexer_peek(&lexer) != '\n') lexer.cursor++;
-        } else if (mos_lexer_peek(&lexer) == ':') {
+        } else if (mos_lexer_peek(lexer) == ':') {
             // end of the label
-            lexer.label_end = lexer.cursor++; // store and increment
+            lexer->label_end = lexer->cursor++; // store and increment
         } else {
-            buf[cursor++] = mos_lexer_get_char(&lexer);
-            //MOS_Token token = mos_create_token(operand, 1, MOS_TOKEN_OPERAND);
-            //mos_lexer_append_token(&lexer, token);
-            //continue;
+            buf[cursor++] = mos_lexer_get_char(lexer);
         }
     }
-    mos_lexer_dump_tokens(&lexer);
+    mos_lexer_dump_tokens(lexer);
     return 0;
 }
+
+// TODO: ERROR Reporting, locations and offsets
